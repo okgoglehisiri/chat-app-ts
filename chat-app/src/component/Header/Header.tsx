dc062402296d8a951325590e8235698b9f3e71f9
okgoglehisiri <okgoglehisiri@okgoglehisiris-Laptop.local>
2024-09-01 15:12:56 +0900

    [feat] Logout function

--- chat-app/src/component/Header/Header.tsx
+++ chat-app/src/component/Header/Header.tsx
@@ -1,13 +1,52 @@
-import { chakra, Container, Heading } from "@chakra-ui/react";
+import { Button, chakra, Container, Heading, useToast } from "@chakra-ui/react";
 import { useAuthContext } from "@src/feature/auth/provider/AuthProvider";
+import { FirebaseError } from "@firebase/util";
+import { getAuth, signOut } from "firebase/auth";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export const Header = () => {
 	const { user } = useAuthContext();
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const toast = useToast();
+	const { push } = useRouter();
+
+	const handleSignOut = async () => {
+		setIsLoading(true);
+		try {
+			const auth = getAuth();
+			await signOut(auth);
+			toast({
+				title: "Logged out.",
+				status: "success",
+				position: "top",
+			});
+			push("/signin");
+		} catch (e) {
+			if (e instanceof FirebaseError) {
+				console.log(e);
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<chakra.header py={4} bgColor={"blue.600"}>
 			<Container maxW={"container.lg"}>
-				<Heading color={"white"}>{user ? "Hello" : "Sign In"}</Heading>
+				<Heading color={"white"}>
+					{user ? (
+						<Button
+							colorScheme={"teal"}
+							onClick={handleSignOut}
+							isLoading={isLoading}
+						>
+							Sign Out
+						</Button>
+					) : (
+						"Sign In"
+					)}
+				</Heading>
 			</Container>
 		</chakra.header>
 	);
