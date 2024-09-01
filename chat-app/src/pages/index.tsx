dfad1cebffcb59dde83f5c458481bee3e22e6b98
okgoglehisiri <okgoglehisiri@okgoglehisiris-Laptop.local>
2024-09-01 14:07:36 +0900

    [feat] SignUp SignIn function implemented.

---
+++ chat-app/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import { Box, Heading } from '@chakra-ui/react'
+const Page: NextPage = () => {
+  return (
+      <Box>
+        <Heading>Chakra UI</Heading>
+        </Box>
+    )
+}
+
+export default Page
---
+++ chat-app/src/pages/signup/index.tsx
@@ -0,0 +1,98 @@
+import {
+	Box,
+	Button,
+	Center,
+	chakra,
+	Container,
+	FormControl,
+	FormLabel,
+	Grid,
+	Heading,
+	Input,
+	Spacer,
+	useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import type { FormEvent } from "react";
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	sendEmailVerification,
+} from "firebase/auth";
+import { FirebaseError } from "@firebase/util";
+
+export const Page = () => {
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const toast = useToast();
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		setIsLoading(true);
+		e.preventDefault();
+		try {
+			const auth = getAuth();
+			const userCredential = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password,
+			);
+			await sendEmailVerification(userCredential.user);
+			setEmail("");
+			setPassword("");
+		} catch (e) {
+			toast({
+				title: "エラーが発生しました。",
+				status: "error",
+				position: "top",
+			});
+			if (e instanceof FirebaseError) {
+				console.log(e);
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	};
+	return (
+		<Container py={14}>
+			<Heading>Sign Up</Heading>
+			<chakra.form onSubmit={handleSubmit}>
+				<Spacer height={8} aria-hidden />
+				<Grid gap={4}>
+					<Box display={"contents"}>
+						<FormControl>
+							<FormLabel>email</FormLabel>
+							<Input
+								type={"email"}
+								name={"email"}
+								value={email}
+								onChange={(e) => {
+									setEmail(e.target.value);
+								}}
+							/>
+						</FormControl>
+						<FormControl>
+							<FormLabel>password</FormLabel>
+							<Input
+								type={"password"}
+								name={"password"}
+								value={password}
+								onChange={(e) => {
+									setPassword(e.target.value);
+								}}
+							/>
+						</FormControl>
+					</Box>
+				</Grid>
+				<Spacer height={4} aria-hidden />
+				<Center>
+					<Button type={"submit"} isLoading={isLoading}>
+						Register Account
+					</Button>
+				</Center>
+			</chakra.form>
+		</Container>
+	);
+};
+
+export default Page;
